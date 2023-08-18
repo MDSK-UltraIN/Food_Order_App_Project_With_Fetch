7f0f0aeeb9f2cbc24f86a9062a11941285d418bf
TW-IT-Jouven Liao <jouven.liao@shoalter.com>
2023-08-18 15:27:36 +0800

    feat: Add fetching to Firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from 'react';
 
 const DUMMY_MEALS = [
   {
@@ -29,8 +30,60 @@ const DUMMY_MEALS = [
   },
 ];
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    }
+
+
+
+    fetchMeals().catch(error => {
+      console.log('fetch failed');
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+
+  }, []);
+
+  if (isLoading) {
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
